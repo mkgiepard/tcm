60360f915c524d2398e355a0f5b7bea1e8880824
mario <mkgiepard@gmail.com>
2019-03-12 22:30:56 +0100

    tcm-be: using express.Router() in the server

--- tcm-be/server.js
+++ tcm-be/server.js
@@ -1,6 +1,28 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
+import mongoose from 'mongoose';
 
+// Define app and router
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Attach cors and bodyParser to the app
+app.use(cors());
+app.use(bodyParser.json());
+
+// Configure mongoose connection
+// mongoose.connect();
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log('MongoDB connected!');
+// });
+
+// Attach router
+app.use('/', router);
+
+// Configure server to listen on port 4000
 app.listen(4000, () => console.log('Express server running'));
+
+// Dummy response
+app.get('/', (req, res) => res.send('dummy hello world!'));
