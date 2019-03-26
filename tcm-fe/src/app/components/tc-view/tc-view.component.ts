0d1685c08c55108bf2aa644090c87250285b7b52
mkgiepard <mkgiepard@gmail.com>
2019-03-26 22:53:53 +0100

    tcm-fe: connecting tc-view componnent to the service

--- tcm-fe/src/app/components/tc-view/tc-view.component.ts
+++ tcm-fe/src/app/components/tc-view/tc-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TestCaseService } from '../../testcase.service';
 
 @Component({
   selector: 'app-tc-view',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TcViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tcService: TestCaseService) { }
 
   ngOnInit() {
+    // Just for testing
+    this.tcService.getTestCases().subscribe((testcases) => {
+      console.log(testcases);
+    });
   }
-
 }
