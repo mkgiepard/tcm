77030fa7424f604c526c0f5a02bbd633cbea95ac
mkgiepard <mkgiepard@gmail.com>
2019-04-14 14:59:37 +0200

    tcm-fe: test plan create and edit components (data, html, css)

--- tcm-fe/src/app/components/tp-create/tp-create.component.ts
+++ tcm-fe/src/app/components/tp-create/tp-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TestPlanService } from '../../testplan.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tp-create',
@@ -7,10 +9,26 @@ import { TestPlanService } from '../../testplan.service';
   styleUrls: ['./tp-create.component.css']
 })
 export class TpCreateComponent implements OnInit {
+  createForm: FormGroup;
 
-  constructor(private tpService: TestPlanService) { }
+  constructor(private tpService: TestPlanService,
+              private fb: FormBuilder,
+              private router: Router) {
+    this.createForm = this.fb.group({
+      title: ["", Validators.required],
+      author: "",
+      desc: "",
+    });
+}
 
-  ngOnInit() {
+ngOnInit() {
+}
+
+addTestPlan(title, author, desc) {
+  this.tpService.addTestPlan(title, author, desc)
+                .subscribe(() => {
+                  this.router.navigate(['/tp-list']);
+                });
   }
 
 }
--- tcm-fe/src/app/components/tp-edit/tp-edit.component.ts
+++ tcm-fe/src/app/components/tp-edit/tp-edit.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TestPlanService } from '../../testplan.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+
+import { TestPlan } from '../../testplan.model';
 
 @Component({
   selector: 'app-tp-edit',
@@ -7,10 +12,48 @@ import { TestPlanService } from '../../testplan.service';
   styleUrls: ['./tp-edit.component.css']
 })
 export class TpEditComponent implements OnInit {
+  id: String;
+  testplan: any = {};
+  updateForm: FormGroup;
 
-  constructor(private tpService: TestPlanService) { }
+  constructor(private tpService: TestPlanService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private snackBar: MatSnackBar,
+              private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params.id;
+      this.tpService.getTestPlanById(this.id).subscribe(res => {
+        this.testplan = res;
+        this.updateForm.get('title').setValue(this.testplan.title);
+        this.updateForm.get('author').setValue(this.testplan.author);
+        this.updateForm.get('desc').setValue(this.testplan.desc);
+        this.updateForm.get('created').setValue(this.testplan.created);
+        this.updateForm.get('updated').setValue(this.testplan.updated);
+      });
+    });
   }
+
+  createForm() {
+    this.updateForm = this.fb.group({
+      title: ["", Validators.required],
+      author: "",
+      desc: "",
+      created: "",
+      updated: "",
+    });
+  }
+
+  updateTestPlan(title, author, desc, create, updated) {
+    this.tpService.updateTestPlan(this.id, title, author, desc)
+                  .subscribe(() => {
+        this.snackBar.open('TestPlan update -- OK', 'OK', { duration:3000 });
+        this.router.navigate(['/tp-list']);
+      });
+    }
 
 }
