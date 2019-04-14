559e499d8433fd085cf5d3fb28da34ef62b68530
mkgiepard <mkgiepard@gmail.com>
2019-04-14 14:38:34 +0200

    tcm-fe: test plan list component (data, html, css)

--- tcm-fe/src/app/components/tp-list/tp-list.component.ts
+++ tcm-fe/src/app/components/tp-list/tp-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TestPlanService } from '../../testplan.service';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
+import { TestPlan } from '../../testplan.model';
 
 @Component({
   selector: 'app-tp-list',
@@ -7,10 +10,33 @@ import { TestPlanService } from '../../testplan.service';
   styleUrls: ['./tp-list.component.css']
 })
 export class TpListComponent implements OnInit {
+  testplans: TestPlan[];
+  displayedColumns = ['title', 'author', 'desc', 'created', 'updated', 'actions'];
 
-  constructor(private tpService: TestPlanService) { }
+  constructor(private tpService: TestPlanService, private router: Router) { }
 
   ngOnInit() {
+    this.fetchTestPlans();
+  }
+
+  fetchTestPlans() {
+    this.tpService
+      .getTestPlans()
+      .subscribe((data: TestPlan[]) => {
+        this.testplans = data;
+        console.log('Data requested...');
+        console.log(this.testplans);
+      });
+  }
+
+  editTestPlan(id) {
+    this.router.navigate([`/tp-edit/${id}`]);
+  }
+
+  deleteTestPlan(id) {
+    this.tpService.deleteTestPlan(id).subscribe(() => {
+      this.fetchTestPlans();
+    });
   }
 
 }
